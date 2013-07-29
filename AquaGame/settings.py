fcce9988f093e15af3ea6ecfdd588ac47638a874
gmartinvela <gustavo.martin@opendeusto.es>
2013-07-29 12:03:37 +0200

    Initial configuration settings.py

--- AquaGame/settings.py
+++ AquaGame/settings.py
@@ -7,15 +7,20 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+# LOGIN PAGES
+#LOGIN_REDIRECT_URL = 'index'
+#LOGIN_URL = '/cuentas/login/'
+#LOGOUT_URL = '/cuentas/logout/'
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'AquaGame',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'letmein',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -50,12 +55,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/weblord/Desktop/AquaGame/AquaGame/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,6 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/weblord/Desktop/AquaGame/AquaGame/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +117,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/weblord/Desktop/AquaGame/AquaGame/templates",
 )
 
 INSTALLED_APPS = (
