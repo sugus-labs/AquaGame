cd211de3f6fea9434b731977b101ede4ebf1e2eb
gmartinvela <gustavo.martin@opendeusto.es>
2013-07-29 15:17:12 +0200

    Added draggable elements to the index template

--- AquaGame/urls.py
+++ AquaGame/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 
+# To serve MEDIA in development versions
+from django.conf import settings
+from django.conf.urls.static import static
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +18,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
