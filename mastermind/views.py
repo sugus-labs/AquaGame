d59dde93c7903e3241748bd93474f66faad78ee5
gmartinvela <gustavo.martin@opendeusto.es>
2013-08-02 15:26:03 +0200

    Fixed index template to view in the index URL

--- mastermind/views.py
+++ mastermind/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mastermind/index.html')
 
-def basic_page(request):
-    return render(request, 'mastermind/basic_page.html')
+def basic_game(request):
+    return render(request, 'mastermind/basic_game.html')
