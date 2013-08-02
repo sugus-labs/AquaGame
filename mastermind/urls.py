7f61b479c7ffcf74ae5c1fa30b8ea624043a237e
gmartinvela <gustavo.martin@opendeusto.es>
2013-08-02 15:12:50 +0200

    Changed and added new index template to have two different interface, the basic one and the normal

--- mastermind/urls.py
+++ mastermind/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^', 'mastermind.views.index', name='index'),
+    url(r'^basic/$', 'mastermind.views.basic_page', name='basic_page'),
     #url(r'^accounts/login/','django.contrib.auth.views.login', {'template_name': 'registration/login_bootstrapped.html'}),
     #url(r'^accounts/logout/','django.contrib.auth.views.logout', {'template_name': 'registration/logged_out_bootstrapped.html'}),
 )
--- mastermind/views.py
+++ mastermind/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mastermind/index.html')
+
+def basic_page(request):
+    return render(request, 'mastermind/basic_page.html')
