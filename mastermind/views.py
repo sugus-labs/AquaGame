f046aa90398d64141be516a47dff608bed2366d1
gmartinvela <gustavo.martin@opendeusto.es>
2013-08-04 13:37:42 +0200

    Added query to DB to obtain all the records of the ranking template

--- mastermind/views.py
+++ mastermind/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from mastermind.models import Ball, Participant
 
 def index(request):
     return render(request, 'mastermind/index.html')
@@ -7,4 +8,7 @@ def basic_game(request):
     return render(request, 'mastermind/basic_game.html')
 
 def ranking(request):
-    return render(request, 'mastermind/ranking.html')
+	participant_list = Participant.objects.order_by('time_needed')
+	context = {'participant_list': participant_list}
+	print participant_list
+	return render(request, 'mastermind/ranking.html', context)
