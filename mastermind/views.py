0624e04bf94bce24386f9c13c622ee44f35f11b0
gmartinvela <gustavo.martin@opendeusto.es>
2013-08-04 22:54:47 +0200

    Added way to highlight the own record after the game

--- mastermind/views.py
+++ mastermind/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from mastermind.models import Ball, Participant
 from django.utils import timezone
+
+def return_all_participants_list():
+	participant_list = Participant.objects.order_by('time_needed')
+	context = {'participant_list': participant_list}
+	return context
 
 def index(request):
     return render(request, 'mastermind/index.html')
@@ -14,10 +20,11 @@ def basic_game(request):
 		participant = Participant(nick_name=nickname, time_needed=time, date_created=timezone.now())
 		participant.save()
 		participant_id = participant.id
-		return redirect('ranking')
+		context = return_all_participants_list()
+		context['participant_id'] = participant_id
+		print context
+		return render(request, 'mastermind/ranking.html', context)
 
 def ranking(request):
-	participant_list = Participant.objects.order_by('time_needed')
-	context = {'participant_list': participant_list}
-	print participant_list
+	context = return_all_participants_list()
 	return render(request, 'mastermind/ranking.html', context)
