939cae296461b02a86838c7af48994cb39eb10c5
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-02 16:20:27 +0200

    Created a new template for a normal game

--- mastermind/urls.py
+++ mastermind/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^index', 'mastermind.views.index', name='index'),
     url(r'^basic', 'mastermind.views.basic_game', name='basic_game'),
+    url(r'^normal', 'mastermind.views.basic_game', name='normal_game'),
     url(r'^ranking', 'mastermind.views.ranking', name='ranking'),
     url(r'^balls_data', 'mastermind.views.insert_balls_data', name='insert_balls_data'),
     #url(r'^accounts/login/','django.contrib.auth.views.login', {'template_name': 'registration/login_bootstrapped.html'}),
--- mastermind/views.py
+++ mastermind/views.py
@@ -25,6 +25,19 @@ def basic_game(request):
 		context['participant_id'] = participant_id
 		return render(request, 'mastermind/ranking.html', context)
 
+def basic_game(request):
+	if request.method == 'GET':
+		return render(request, 'mastermind/normal_game.html')
+	else:
+		nickname = request.POST['nickname']
+		time = request.POST['time_needed']
+		participant = Participant(nick_name=nickname, time_needed=time, date_created=timezone.now())
+		participant.save()
+		participant_id = participant.id
+		context = return_all_participants_list()
+		context['participant_id'] = participant_id
+		return render(request, 'mastermind/ranking.html', context)
+
 def ranking(request):
 	context = return_all_participants_list()
 	return render(request, 'mastermind/ranking.html', context)
