b63ca7ffed524def181daa3ed6c0217b32d4953f
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-02 17:18:32 +0200

    Enhanced ball model for the future

--- mastermind/models.py
+++ mastermind/models.py
@@ -13,6 +13,14 @@ class Ball(models.Model):
 		help_text = "Write the name of the liquid inside the ball.")
 	liquid_contained_density = models.FloatField(
 		help_text = "Write the density of the liquid inside the ball. The format must be '1.24' or '0.567'.")
+	# example_image = models.ImageField(upload_to = 'admin_ball_images', 
+	#	help_text = "This image helps you to recognize the ball in the water")
+	# ball_weight = models.FloatField(
+	#	help_text = "Write the weight of the ball without the liquid. The format must be '1.24' or '0.567'.")
+	# ball_total_weight = models.FloatField(
+	#	help_text = "Write the weight of the ball with the liquid. The format must be '1.24' or '0.567'.")
+	# ball_diameter = models.FloatField(
+	#	help_text = "Write the diameter of the ball. The format must be '1.24' or '0.567'.")
 	date_refreshed = models.DateTimeField(auto_now=True)
 	date_created = models.DateTimeField(auto_now_add=True)
 
