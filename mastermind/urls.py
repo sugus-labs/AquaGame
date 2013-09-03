a3b18b91ed78f8f93ae2e9ba8a0685ba87e22a03
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-03 16:52:58 +0200

    Some changes to enhance the user experience

--- mastermind/urls.py
+++ mastermind/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^index', 'mastermind.views.index', name='index'),
     url(r'^basic', 'mastermind.views.basic_game', name='basic_game'),
     url(r'^normal', 'mastermind.views.normal_game', name='normal_game'),
     url(r'^ranking', 'mastermind.views.ranking', name='ranking'),
     url(r'^balls_data', 'mastermind.views.insert_balls_data', name='insert_balls_data'),
     #url(r'^accounts/login/','django.contrib.auth.views.login', {'template_name': 'registration/login_bootstrapped.html'}),
     #url(r'^accounts/logout/','django.contrib.auth.views.logout', {'template_name': 'registration/logged_out_bootstrapped.html'}),
+    url(r'', 'mastermind.views.index', name='index'),
 )
