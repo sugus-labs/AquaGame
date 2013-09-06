017e87c3a09d433c27b75decd0de097b068b7735
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-06 10:08:44 +0200

    Randomize the outputs of the liquids in views. TODO: Print in templates dinamically

--- mastermind/views.py
+++ mastermind/views.py
@@ -4,6 +4,7 @@ from mastermind.models import Ball, Participant, BallForm
 from django.utils import timezone
 from django.forms.models import modelformset_factory
 import json
+import random
 
 attempts_num = 4
 
@@ -16,10 +17,12 @@ def return_all_participants_list():
 
 def return_balls_to_HTML():
 	balls_db = Ball.objects.all().reverse()
-	balls_colour_list = ["",]
+	balls_colour_list = []
 	for ball in balls_db:
 		print ball.get_colour_display()
 		balls_colour_list.append('ball_' + ball.get_colour_display())
+	random.shuffle(balls_colour_list)
+	balls_colour_list.insert(0, "")
 	balls_json = json.dumps([unicode(ball) for ball in balls_colour_list])
 	return balls_json, balls_db
 
