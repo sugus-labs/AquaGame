b543ebd786ae04e4442df988f802f85b782fc174
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-09 10:25:05 +0200

    FIXED crash when running supervisord putting absolute paths.

--- AquaGame/settings.py
+++ AquaGame/settings.py
@@ -80,7 +80,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ROOT_PATH +'/AquaGame/static',
+    '/home/weblord/Desktop/AquaGame/AquaGame/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ROOT_PATH + '/AquaGame/templates',
+    '/home/weblord/Desktop/AquaGame/AquaGame/templates',
 )
 
 INSTALLED_APPS = (
